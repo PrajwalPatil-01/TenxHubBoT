f342b2b6badfe04421b48e84f31274f2179c0807
SujanChadri <sujanchy549@gmail.com>
2025-12-22 12:59:19 +0545

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '')  # Channel/Group ID for force sub
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002551525608')  # Support group id (make sure bot is admin)
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001767829947')  # Request channel id (make sure bot is admin)
-AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '').split()]
+AUTH_CHANNEL = [int(fch) if id_pattern.search(fch) else fch for fch in environ.get('AUTH_CHANNEL', '-1001796707878').split()]
 MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '').split() if re.match(r'^-?\d+$', channel_id)]  # Channel for force sub (make sure bot is admin)
 # ============================
 # Payment Configuration
